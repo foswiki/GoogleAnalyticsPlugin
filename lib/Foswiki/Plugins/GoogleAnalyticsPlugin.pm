f7bce54cebfb889df29ad7ab8a46a5beeb500c92
WillNorris <WillNorris@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-02-25 07:03:02 +0000

    Item1142: with proper quoting, this time
    git-svn-id: http://svn.foswiki.org/trunk/GoogleAnalyticsPlugin@2701 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/GoogleAnalyticsPlugin.pm
+++ lib/Foswiki/Plugins/GoogleAnalyticsPlugin.pm
@@ -33,22 +33,22 @@ sub initPlugin {
 ################################################################################
 
 sub _addToHead {
-    my $header = '<!-- Google Analytics script -->
+    my $header = q|<!-- Google Analytics script -->
 <script type="text/javascript">
 // <![CDATA[
-    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+	    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 </script>
 <script type="text/javascript">
-try {
-    var pageTracker = _gat._getTracker("'.$googleSiteKey.'");
-    pageTracker._trackPageview();
-} catch(err) {}
+	    try {
+		var pageTracker = _gat._getTracker("|.$googleSiteKey.q|");
+		pageTracker._trackPageview();
+	    } catch(err) {}
 // ]]>
-</script>
-';
-    Foswiki::Func::addToHEAD('GOOGLEANALYTICSPLUGIN',$header)
-}
+    </script>|;
+
+  Foswiki::Func::addToHEAD('GOOGLEANALYTICSPLUGIN',$header)
+  }
 
 ################################################################################
 1;
